f9d8d53ed71e4acf7e7d44b11f642c7965e0dd0e
karnamin <aminkarn18@gmail.com>
2021-03-19 01:51:36 +0530

    added react-router and route pages skeleton

--- src/Pages/Navbar.js
+++ src/Pages/Navbar.js
@@ -1,24 +1,37 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+
 import '../Styles/Navbar.css'
 import logo from '../Resources/Logo.svg'
+
+import Homepage from './Homepage';
+import AboutUs from './AboutUs';
+import ContactUs from './ContactUs';
 
 class Navbar extends Component {
     render() {
         return (
-            <div id='navbar'>
-                <ul id='nav'>
-                    <div className='nav-left'>
-                        <li><img src={logo} alt="logo"/></li>
-                    </div>
-                    <div className='nav-right'>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">About Us</a></li>
-                        <li><a href="#">Contact Us</a></li>
-                        <li><a href="#">Log In</a></li>
-                    </div>
-                    
-                </ul>
-            </div>
+            <Router>
+                <div id='navbar'>
+                    <ul id='nav'>
+                        <div className='nav-left'>
+                            <li><img src={logo} alt="logo"/></li>
+                        </div>
+                        <div className='nav-right'>
+                            <li><Link to='/'>Home</Link></li>
+                            <li><Link to='/about-us'>About Us</Link></li>
+                            <li><Link to='/contact-us'>Contact Us</Link></li>
+                            <li><a href="#">Log In</a></li>
+                        </div>
+                        
+                    </ul>
+                </div>
+                <Switch>
+                    <Route exact path='/' component={Homepage} />
+                    <Route exact path='/about-us' component={AboutUs} />
+                    <Route exact path='/contact-us' component={ContactUs} />
+                </Switch>
+            </Router>
         )
     }
 }
