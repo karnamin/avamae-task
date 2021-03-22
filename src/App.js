b500f329516379dad7d21e7816232add106b59e0
karnamin <aminkarn18@gmail.com>
2021-03-22 08:00:04 +0530

    added Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './Pages/Navbar';
+import Footer from './Pages/Footer';
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
+			<Footer />
 		</div>
   	);
 }
---
+++ src/Pages/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import '../Styles/Footer.css'
+
+class Footer extends Component {
+    render() {
+        return (
+            <div className='Footer'>
+                <p>Website Development by AVAMAE</p>
+            </div>
+        )
+    }
+}
+
+export default Footer
